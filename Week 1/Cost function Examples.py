cafe92084b7ecee5d661e564b86763216283824f
Negar Loloshahvar <negarshahvar@gmail.com>
2022-12-22 15:30:02 +0000

    Delete Cost function Examples.py

--- Week 1/Cost function Examples.py
+++
@@ -1,6 +0,0 @@
-# We are going to implement and explore the cost function for linear regression with one variable.
-
-import numpy as np
-import matplotlib.pyplot as plt
-from lab_utils_uni import plt_intuition, plt_stationary, plt_update_onclick, soup_bowl
-plt.style.use('./deeplearning.mplstyle')
