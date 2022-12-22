5d62d95571b70fec738b68c584373e39a2866e13
Negar Loloshahvar <negarshahvar@gmail.com>
2022-12-22 15:18:10 +0000

    update

---
+++ Week 1/Cost function Examples.py
@@ -0,0 +1,7 @@
+# We are going to implement and explore the cost function for linear regression with one variable.
+
+import numpy as np
+import matplotlib.pyplot as plt
+%matplotlib widget
+from lab_utils_uni import plt_intuition, plt_stationary, plt_update_onclick, soup_bowl
+
